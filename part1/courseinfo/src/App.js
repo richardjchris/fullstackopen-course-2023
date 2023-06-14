fc8a46ece9acef48793cd02d4b1a4cd9f16b3fe7
Richard Christiansen <richard.jchristiansen@gmail.com>
2023-06-14 16:42:32 +1000

    Updated app to second part specifications

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,49 +1,63 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const course_content = [
-    { part: 'Fundamentals of React', exercises: 10},
-    { part: 'Using props to pass data', exercises: 7},
-    { part: 'State of a component', exercises: 14}
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content course_content={course_content} />
-      <Total count={exercises1 + exercises2 + exercises3} />
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
 
-const Header = (header) => (
-  <h1>{header.course}</h1>
+const Header = (props) => (
+  <h1>{props.course.name}</h1>
 )
 
 const Content = (props) => {
-  const course_content = props.course_content
+  const course_content = props.course.parts
+
+  const Part = (props) => (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+
   return (
     <div>
-      <Part part={course_content[0].part} exercises={course_content[0].exercises} />
-      <Part part={course_content[1].part} exercises={course_content[1].exercises} />
-      <Part part={course_content[2].part} exercises={course_content[2].exercises} />
+      <Part part={course_content[0].name} exercises={course_content[0].exercises} />
+      <Part part={course_content[1].name} exercises={course_content[1].exercises} />
+      <Part part={course_content[2].name} exercises={course_content[2].exercises} />
     </div>
   )
 }
 
-const Part = (props) => (
-  <p>
-    {props.part} {props.exercises}
-  </p>
-)
+const Total = (props) => {
+  let total_exercises = 0
 
-const Total = (props) => (
-  <p>Number of exercises {props.count}</p>
-)
+  props.course.parts.forEach(value => {
+    total_exercises += value.exercises
+  })
+
+  return (
+    <p>Number of exercises {total_exercises}</p>
+  )
+}
 
 export default App
